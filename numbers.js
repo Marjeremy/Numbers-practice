086dec54189ceba7f5a845e331488f3a9ef24ca3
Marjeremy <gaoat856@gmail.com>
2024-09-14 08:09:34 +0800

    Numbers-practice commit

---
+++ numbers.js
@@ -0,0 +1,15 @@
+let firstFavNum = 20;
+let secondFavNum = 10;
+
+let addition = firstFavNum + secondFavNum;
+let subtraction = firstFavNum - secondFavNum;
+let multiplication = firstFavNum * secondFavNum;
+let division = firstFavNum / secondFavNum;
+let modulos = firstFavNum % secondFavNum;
+let exponentiation = Math.pow(firstFavNum, secondFavNum);
+
+console.log("Addition:", addition);
+console.log("subtraction:", subtraction);
+console.log("Multiplication:", multiplication);
+console.log("Modulos:", modulos);
+console.log("Exponentiation :", exponentiation);
